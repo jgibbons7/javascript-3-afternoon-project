5b6139abc93d07ed71122038ebf52e142492fc33
jgibbons7 <gibbons.josh@outlook.com>
2020-05-04 08:56:18 -0600

    updated

--- higher-order.js
+++ higher-order.js
@@ -117,7 +117,9 @@ const orders = [{"price":15,"tax":0.09},{"price":42,"tax":0.07},{"price":56,"tax
   Use a higher order method to get all the order totals after adding in the sales tax (given to you as a tax rate, hint: you'll need to do some multiplication). Your answer should be an array of numbers, one total for each order.
 */
 
-let orderTotals // Code here
+let orderTotals = orders.reduce((acc, element, index, array) => {
+  price * tax + price
+}, 0)
 
 
 
